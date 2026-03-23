ed54d16f33665312a97ce949749661ff901440b9
rahulkulk29 <rahulkulkarni1929@gmail.com>
2026-03-23 20:47:13 +0530

    all in one

--- frontend/src/modules/shared/utils/api.js
+++ frontend/src/modules/shared/utils/api.js
@@ -1,6 +1,6 @@
 import { auth } from '@/modules/shared/config/firebase';
 
-export const API_BASE = 'http://localhost:5000';
+export const API_BASE = 'http://localhost:5001';
 
 /**
  * Authenticated fetch wrapper.
