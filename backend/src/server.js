11c285dfec74ea65e0faf9912e4e582c37712193
rahulkulk29 <rahulkulkarni1929@gmail.com>
2026-05-01 16:14:40 +0530

    url shift

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['https://goodkart.onrender.com', 'http://localhost:5173', 'http://localhost:3000'],
+    origin: [process.env.FRONTEND_URL, 'https://sellsathifrontend.onrender.com', 'http://localhost:5173', 'http://localhost:3000'].filter(Boolean),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-uid', 'x-role', 'X-Test-UID']
